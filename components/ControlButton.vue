1596d92e03190a644466adf97dca01dad7778b4f
Flo <tectoast@gmail.com>
2024-08-13 21:41:52 +0200

    feat: propagate disable property of ControlButton

--- components/ControlButton.vue
+++ components/ControlButton.vue
@@ -1,5 +1,12 @@
 <template>
-  <button class="bg-[#3D8CD5] text-white text-xl min-w-24 min-h-10 rounded-lg flex justify-center items-center px-4">
+  <button :disabled="props.disabled"
+    class="bg-[#3D8CD5] text-white text-xl min-w-24 min-h-10 rounded-lg flex justify-center items-center px-4">
     <slot></slot>
   </button>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  disabled?: boolean
+}>()
+</script>
