f97b3d41a6a1adbe00d27f4bee0a515c816eb955
Flo <tectoast@gmail.com>
2024-10-04 15:51:02 +0200

    feat: default dark background

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt"],
+  app: {
+    head: {
+      htmlAttrs: {
+        style: "background-color: #36393f",
+      },
+    },
+  },
   routeRules: {
     "/api/**": {
       proxy: {
