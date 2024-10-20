8e2b22696c7238007630effd2bc10b4bcefeb419
Flo <tectoast@gmail.com>
2024-10-20 13:14:04 +0200

    chore: prettier run

--- composables/wizard/specialRoles.ts
+++ composables/wizard/specialRoles.ts
@@ -2,21 +2,21 @@ import { useWizardConnection } from "~/composables/wizard/useWizardConnection";
 import { useWebsocketRef } from "~/utils/wsutils";
 
 export function useSpecialRoles() {
-    const { data, sendWS } = useWizardConnection();
-    const { result: playerRoles } = useWebsocketRef<{
-        [player: string]: string;
-    }>(data, "SelectedRoles", "roles", {});
+  const { data, sendWS } = useWizardConnection();
+  const { result: playerRoles } = useWebsocketRef<{
+    [player: string]: string;
+  }>(data, "SelectedRoles", "roles", {});
 
-    function requestSelectedRole(roleName: string) {
-        sendWS("RequestSelectedRole", {roleName})
-    }
+  function requestSelectedRole(roleName: string) {
+    sendWS("RequestSelectedRole", { roleName });
+  }
 
-    const { result: currentRoleSelectingPlayer } = useWebsocketRef(
-        data,
-        "CurrentRoleSelectingPlayer",
-        "currentPlayer",
-        "",
-      );
+  const { result: currentRoleSelectingPlayer } = useWebsocketRef(
+    data,
+    "CurrentRoleSelectingPlayer",
+    "currentPlayer",
+    "",
+  );
 
-    return {playerRoles, requestSelectedRole, currentRoleSelectingPlayer};
+  return { playerRoles, requestSelectedRole, currentRoleSelectingPlayer };
 }
--- pages/wizard/game/[id].client.vue
+++ pages/wizard/game/[id].client.vue
@@ -57,9 +57,16 @@ const isSelectColorModalActive = computed(() => selectColorCard.value != null);
 const { isChangeStitchModalActive, changeStitchPrediction } =
   useChangeStitchPrediction();
 
-const { playerRoles, requestSelectedRole, currentRoleSelectingPlayer } = useSpecialRoles()
-const isRoleSelectionModalOpen = computed(() => currentRoleSelectingPlayer.value == playerName.value)
-const isWaitingForOtherPlayersRoleSelectionModalOpen = computed(() => currentRoleSelectingPlayer.value != "" && currentRoleSelectingPlayer.value != playerName.value)
+const { playerRoles, requestSelectedRole, currentRoleSelectingPlayer } =
+  useSpecialRoles();
+const isRoleSelectionModalOpen = computed(
+  () => currentRoleSelectingPlayer.value == playerName.value,
+);
+const isWaitingForOtherPlayersRoleSelectionModalOpen = computed(
+  () =>
+    currentRoleSelectingPlayer.value != "" &&
+    currentRoleSelectingPlayer.value != playerName.value,
+);
 
 watchMessage(data, "RedirectHome", () => {
   navigateTo("/wizard");
@@ -95,7 +102,6 @@ useHead({
   })),
 });
 
-
 watchMessage(data, "ClearForNewSubRound", () => {
   layedCards.value = layedCards.value.map((x) => ({
     player: x.player,
@@ -116,12 +122,15 @@ const selectChangeCardState = ref<SelectChangeCard>("nothing");
 watchMessage(data, "SevenPointFiveUsed", () => {
   selectChangeCardState.value = "selectCard";
 });
-definePageMeta({colorMode: 'dark'})
+definePageMeta({ colorMode: "dark" });
 </script>
 
 <template>
   <DefaultBackground>
-    <div v-if="gamephase === 'lobby'" class="mt-20 flex flex-row justify-center">
+    <div
+      v-if="gamephase === 'lobby'"
+      class="mt-20 flex flex-row justify-center"
+    >
       <div class="w-1/3">
         <p class="mb-3 text-center text-3xl text-gray-300">Spieler</p>
         <ul class="divide-y-2 divide-gray-100 rounded-lg bg-white shadow">
@@ -129,12 +138,18 @@ definePageMeta({colorMode: 'dark'})
             {{ c + (index == 0 ? " (Owner)" : "") }}
           </li>
         </ul>
-        <button @click="startGame()" v-if="isOwner" :class="noStart ? 'grayscale' : 'grayscale-0'"
-          class="my-4 block w-full rounded bg-blue-500 px-4 py-2 text-white transition-all duration-500 hover:bg-blue-700">
+        <button
+          @click="startGame()"
+          v-if="isOwner"
+          :class="noStart ? 'grayscale' : 'grayscale-0'"
+          class="my-4 block w-full rounded bg-blue-500 px-4 py-2 text-white transition-all duration-500 hover:bg-blue-700"
+        >
           Spiel starten
         </button>
-        <button @click="leaveGame()"
-          class="my-4 block w-full rounded bg-red-500 px-4 py-2 text-white transition-all duration-500 hover:bg-red-700">
+        <button
+          @click="leaveGame()"
+          class="my-4 block w-full rounded bg-red-500 px-4 py-2 text-white transition-all duration-500 hover:bg-red-700"
+        >
           Spiel verlassen
         </button>
 
@@ -143,13 +158,22 @@ definePageMeta({colorMode: 'dark'})
             Spielregeln
           </p>
           <div class="flex flex-col gap-1">
-            <div v-for="(x, index) of Object.keys(Rules)" class="flex flex-auto flex-col">
+            <div
+              v-for="(x, index) of Object.keys(Rules)"
+              class="flex flex-auto flex-col"
+            >
               <hr v-if="index > 0" />
               <p class="mt-3 text-center text-xl text-gray-300">{{ x }}</p>
               <div class="flex flex-row gap-2">
-                <button v-for="r of Rules[x]" @click="switchRule(x, r)" :disabled="!isOwner" :class="rules[x] == r && !notSet ? 'grayscale-0' : 'grayscale'
+                <button
+                  v-for="r of Rules[x]"
+                  @click="switchRule(x, r)"
+                  :disabled="!isOwner"
+                  :class="
+                    rules[x] == r && !notSet ? 'grayscale-0' : 'grayscale'
                   "
-                  class="my-4 block w-full rounded bg-green-500 px-4 py-2 text-white transition-all duration-500 hover:bg-green-700">
+                  class="my-4 block w-full rounded bg-green-500 px-4 py-2 text-white transition-all duration-500 hover:bg-green-700"
+                >
                   {{ r }}
                 </button>
               </div>
@@ -160,15 +184,21 @@ definePageMeta({colorMode: 'dark'})
     </div>
     <div v-if="gamephase === 'game'">
       <div class="absolute right-2 top-2">
-        <button @click="stopGame()"
-          class="max-h-10 rounded-xl border border-red-500 p-2 text-white transition duration-100 hover:cursor-pointer hover:bg-red-500">
+        <button
+          @click="stopGame()"
+          class="max-h-10 rounded-xl border border-red-500 p-2 text-white transition duration-100 hover:cursor-pointer hover:bg-red-500"
+        >
           Spiel beenden
         </button>
       </div>
       <div class="text-center text-lg">
-        <strong class="text-gray-100">Runde {{ round }} - {{ playerName }}</strong>
+        <strong class="text-gray-100"
+          >Runde {{ round }} - {{ playerName }}</strong
+        >
       </div>
-      <div class="absolute top-10 flex min-w-full flex-row justify-evenly align-middle text-gray-100">
+      <div
+        class="absolute top-10 flex min-w-full flex-row justify-evenly align-middle text-gray-100"
+      >
         <div>
           <strong class="text-center">Trumpf{{ trumpShift }}</strong>
           <WizardCard :card="trump" type="trump"></WizardCard>
@@ -187,7 +217,10 @@ definePageMeta({colorMode: 'dark'})
         </p>
         <div class="flex flex-row justify-evenly gap-5 align-middle">
           <div v-for="c of layedCards">
-            <p class="text-center" :class="{ 'border-2 border-pink-500': currentPlayer == c.player }">
+            <p
+              class="text-center"
+              :class="{ 'border-2 border-pink-500': currentPlayer == c.player }"
+            >
               {{
                 c.player +
                 " (" +
@@ -198,20 +231,24 @@ definePageMeta({colorMode: 'dark'})
                 (hasPredicted.includes(c.player) ? " ✅" : "")
               }}
 
-              <span v-if="results[c.player] != undefined" :class="results[c.player] >= 0 ? 'text-green-400' : 'text-red-500'
-                ">
-                {{
-                  (results[c.player] >= 0 ? "+" : "") + results[c.player]
-                }}
+              <span
+                v-if="results[c.player] != undefined"
+                :class="
+                  results[c.player] >= 0 ? 'text-green-400' : 'text-red-500'
+                "
+              >
+                {{ (results[c.player] >= 0 ? "+" : "") + results[c.player] }}
               </span>
-              <br>
+              <br />
               <span class="text-gray-400">
-                {{
-                  playerRoles[c.player]
-                }}
+                {{ playerRoles[c.player] }}
               </span>
             </p>
-            <WizardCard :card="c.card" :type="'layed'" :firstCard="firstCard"></WizardCard>
+            <WizardCard
+              :card="c.card"
+              :type="'layed'"
+              :firstCard="firstCard"
+            ></WizardCard>
           </div>
         </div>
       </div>
@@ -291,29 +328,48 @@ definePageMeta({colorMode: 'dark'})
       <div
         v-if="firstCome !== ''"
         class="absolute flex min-w-full flex-row justify-center text-center align-middle text-2xl font-bold text-gray-100"
-        style="top: 28%">
+        style="top: 28%"
+      >
         <div>Tipp: {{ firstCome }} kommt raus!</div>
       </div>
 
       <form @submit.prevent="saveStitches()" v-if="firstCome != ''">
-        <div class="absolute flex min-w-full flex-row justify-center gap-4 align-middle" style="top: 35%">
+        <div
+          class="absolute flex min-w-full flex-row justify-center gap-4 align-middle"
+          style="top: 35%"
+        >
           <div class="flex flex-col">
             <label for="stitches" class="text-gray-300">Stiche</label>
-            <input v-model="stitchesPredicted" name="stitches"
+            <input
+              v-model="stitchesPredicted"
+              name="stitches"
               class="focus:shadow-outline w-32 appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-              id="stitches" type="number" placeholder="0" min="0" :max="round" />
+              id="stitches"
+              type="number"
+              placeholder="0"
+              min="0"
+              :max="round"
+            />
           </div>
-          <button type="submit" :disabled="!playersTurn"
-            class="mt-auto max-h-10 rounded-xl border p-2 text-white transition duration-100" :class="playersTurn
-              ? 'border-blue-500 hover:cursor-pointer hover:bg-blue-500'
-              : 'border-gray-500'
-              ">
+          <button
+            type="submit"
+            :disabled="!playersTurn"
+            class="mt-auto max-h-10 rounded-xl border p-2 text-white transition duration-100"
+            :class="
+              playersTurn
+                ? 'border-blue-500 hover:cursor-pointer hover:bg-blue-500'
+                : 'border-gray-500'
+            "
+          >
             Bestätigen
           </button>
         </div>
       </form>
-      <div v-if="currentStitchWinner != ''"
-        class="absolute flex min-w-full flex-row justify-center text-center align-middle" style="top: 35%">
+      <div
+        v-if="currentStitchWinner != ''"
+        class="absolute flex min-w-full flex-row justify-center text-center align-middle"
+        style="top: 35%"
+      >
         <p class="text-center text-3xl font-bold text-gray-200">
           Der Stich geht an
           <span :class="{ 'text-[#12abf5]': currentStitchWinner == null }">
@@ -344,7 +400,9 @@ definePageMeta({colorMode: 'dark'})
       </div>
     </div>
     <div v-if="gamephase == 'finished'">
-      <div class="absolute top-1/3 flex min-w-full flex-row justify-evenly align-middle text-gray-100">
+      <div
+        class="absolute top-1/3 flex min-w-full flex-row justify-evenly align-middle text-gray-100"
+      >
         <div class="w-full p-4 md:w-1/2 lg:w-1/3">
           <p class="text-center text-4xl">Rangliste</p>
           <ul class="my-5">
@@ -356,8 +414,10 @@ definePageMeta({colorMode: 'dark'})
               </div>
             </li>
           </ul>
-          <button @click="leaveGame()"
-            class="my-4 block w-full rounded bg-red-500 px-4 py-2 text-white transition-all duration-500 hover:bg-red-700">
+          <button
+            @click="leaveGame()"
+            class="my-4 block w-full rounded bg-red-500 px-4 py-2 text-white transition-all duration-500 hover:bg-red-700"
+          >
             Spiel löschen
           </button>
         </div>
@@ -372,18 +432,35 @@ definePageMeta({colorMode: 'dark'})
           Wähle eine Rolle aus
         </div>
       </template>
-      <div v-for="role of Object.keys(SpecialRolesDescriptions)" class="px-2 py-0">
-        <UTooltip :text="SpecialRolesDescriptions[role]" :popper="{ placement: 'right' }" :ui="{ width: 'max-w-screen-xl' }"
-          class="w-full">
-          <button @click="requestSelectedRole(role)" :class="!Object.values(playerRoles).includes(role) ? 'grayscale-0' : 'grayscale'"
-            class="my-2 w-full rounded bg-green-500 px-4 py-2 text-white transition-all duration-500 hover:bg-green-700">
+      <div
+        v-for="role of Object.keys(SpecialRolesDescriptions)"
+        class="px-2 py-0"
+      >
+        <UTooltip
+          :text="SpecialRolesDescriptions[role]"
+          :popper="{ placement: 'right' }"
+          :ui="{ width: 'max-w-screen-xl' }"
+          class="w-full"
+        >
+          <button
+            @click="requestSelectedRole(role)"
+            :class="
+              !Object.values(playerRoles).includes(role)
+                ? 'grayscale-0'
+                : 'grayscale'
+            "
+            class="my-2 w-full rounded bg-green-500 px-4 py-2 text-white transition-all duration-500 hover:bg-green-700"
+          >
             {{ role }}
           </button>
         </UTooltip>
       </div>
     </UCard>
   </UModal>
-  <UModal v-model="isWaitingForOtherPlayersRoleSelectionModalOpen" prevent-close>
+  <UModal
+    v-model="isWaitingForOtherPlayersRoleSelectionModalOpen"
+    prevent-close
+  >
     <UCard class="px-2 py-0">
       <template #header>
         <div class="text-center text-2xl font-bold text-gray-300">
