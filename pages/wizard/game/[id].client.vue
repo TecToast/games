a109ac23b9df98df2d058e258355429554b9d30f
Tobias <tobias-vaasen@gmx.de>
2025-01-19 20:42:00 +0100

    feat: card tooltips

--- pages/wizard/game/[id].client.vue
+++ pages/wizard/game/[id].client.vue
@@ -302,7 +302,9 @@ function muteSpeaker() {
       >
         <div>
           <strong class="text-center">Trumpf{{ trumpShift }}</strong>
-          <WizardCard :card="trump" type="trump"></WizardCard>
+          <p>
+            <WizardCard :card="trump" type="trump"></WizardCard>
+          </p>
         </div>
         <p
           v-if="selectChangeCardState == 'selectCard'"
