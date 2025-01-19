31a708fd814575c39e9bdaa640370cad4e69eeb5
Tobias <tobias-vaasen@gmx.de>
2025-01-19 22:26:29 +0100

    fix: typo

--- utils/wizard/types.ts
+++ utils/wizard/types.ts
@@ -42,8 +42,8 @@ export const SpecialRolesDescriptions: { [role: string]: string } = {
 
 export const CardsDescriptions: {[cardName: string]:string} = {
   "Narr5" : "Dreht die Legerichtung nach dieser Runde um.",
-  "Narr6" : "Die Karte des nächten Spielers in Legerichtugn wird bei der Wertung ignoriert.",
-  "Narr7" : "Der Gewinner des stiches bleibt gliech, aber die Punkte werden nur allen anderen angerechnet.",
+  "Narr6" : "Die Karte des nächsten Spielers in Legerichtugn wird bei der Wertung ignoriert.",
+  "Narr7" : "Der Gewinner des Stiches bleibt gliech, aber die Punkte werden nur allen anderen angerechnet.",
   "Narr8" : "Die Karte des vorherigen Spielers in Legerichtung wird bei der Wertung ignoriert.",
 
   "Spezial14" : "Der Wert dieses Stiches wird auf 3 gesetzt.",
