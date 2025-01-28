f30c1afa90b2a4b75de0bfcb3822abbb89242b8d
Flo <tectoast@gmail.com>
2025-01-28 14:36:17 +0100

    refactor: better typing for error page

--- pages/error/[[reason]].vue
+++ pages/error/[[reason]].vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-const reasons = {
+const reasons: { [k: string]: string } = {
   notwhitelisted:
     "To be able to login with discord, you must be whitelisted by the admin.",
 };
+const route = useRoute();
 </script>
 
 <template>
   <DefaultBackground>
     <h1 class="mt-8 text-center text-4xl text-white">
       {{
-        reasons[$route.params.reason] ||
+        reasons[route.params.reason.toString()] ||
         "Unknown error occured, please try again later."
       }}
     </h1>
