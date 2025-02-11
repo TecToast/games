d081a01cc4ba639b09286800bfe53d61afffc07b
Flo <tectoast@gmail.com>
2025-02-12 00:07:16 +0100

    feat: configurable host

--- composables/wizard/useWizardConnection.ts
+++ composables/wizard/useWizardConnection.ts
@@ -3,10 +3,11 @@ import type { OpenGamesData } from "~/utils/wizard/types";
 import type { WSMessage } from "~/utils/wizard/messages";
 
 const useWizardConnectionTemplate = () => {
+  const config = useRuntimeConfig();
   const ws = useTypedWebsocket<WSMessage>(
     process.env.NODE_ENV === "development"
       ? `ws://localhost:3000/api/wizard/ws`
-      : `wss://games.tectoast.de/api/wizard/ws`,
+      : `wss://${config.public.host}/api/wizard/ws`,
   );
   const openGames = useState<OpenGamesData>(
     "openGames" /*, () => {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
       protectedUrls: ["/jeopardy", "/wizard", "/nobodyisperfect"],
       userdataRequired: ["/jeopardy", "/nobodyisperfect"],
       wizardYT: false,
+      host: "",
     },
     oauth: {
       discord: {
