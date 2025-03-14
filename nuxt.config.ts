487e11108a8f561f2c073ea92c56a52be2424985
Flo <tectoast@gmail.com>
2025-03-14 22:30:31 +0100

    feat: set cookie for 28 days

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,7 +56,7 @@ export default defineNuxtConfig({
       wizard: "./cards/wizard",
     },
     session: {
-      maxAge: 60 * 60 * 24 * 7,
+      maxAge: 60 * 60 * 24 * 28,
       password: "",
     },
   },
